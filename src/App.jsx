a916b24ce08552e74db5045e879b112f2bfce98a
Juliana Elias <eliasjuliana91@gmail.com>
2024-01-09 11:16:56 -0300

    realizo navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,11 @@ import {
   SuperQuality,
 } from "./sections";
 
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b"><Hero/></section>
     <section className="padding"><PopularProducts/></section>
     <section className="padding"><SuperQuality/></section>
